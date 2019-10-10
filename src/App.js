bbf0ffb7ab7b580a3f16ea99caa3419b5dc2bd49
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-10-10 19:44:29 -0400

    added nav bar, daily inspire and card with drop down

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Data from './components/NASAPhoto/Data';
+import Navbar from "./components/pageNav/Navbar";
 import "./App.css";
 
 function App() {
@@ -7,7 +8,9 @@ function App() {
     <div className="App">
       <div>
         <Data />
+        <Navbar />
       </div>
+      <footer><div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div></footer>
     </div>
   );
 }
--- src/components/NASAPhoto/PhotoCard.js
+++ src/components/NASAPhoto/PhotoCard.js
@@ -1,19 +1,63 @@
 import React from "react";
+import styled from "styled-components";
+import { UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap';
+
+const Thumbnail = styled.div`
+position: relative;
+width: 100%;
+margin-left: auto;
+margin-right: auto;
+margin-top: 10%;
+`;
+
+const Caption = styled.div`
+position: absolute;
+    top: 75%;
+    left: 50%;
+    right: 50%;
+    transform: translate(-50%, -50%);
+    width: 50%;
+    color: lightgrey;
+    font-size: 2.0rem;
+    font-weight: 900;
+    text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+`;
+
+const DropInfo = styled.div`
+width: 40%;
+margin-left: auto;
+margin-right: auto;
+`;
+
 
 
 const PhotoCard = props => {
     return (
         <div className="photo-nasa" >
-            <img src={props.data.url} alt='NASA pick of the day' />
-            <h2>Photo Title: {props.data.title}</h2>
-            <span>Date: 🚀{props.data.date}🚀</span>
-            <p>Explanation:  {props.data.explanation}</p>
+            <Thumbnail>
+                <img src={props.data.url} alt='NASA pick of the day' />
+            </Thumbnail>
+            <DropInfo>
+                <Button color="secondary" id="toggler" style={{ marginBottom: '1rem' }}>
+                    {props.data.title}
+                </Button>
+                <UncontrolledCollapse toggler="#toggler">
+                    <Card>
+                        <CardBody>
+                            <p>{props.data.date}</p>
+                            {props.data.explanation}
+                        </CardBody>
+                    </Card>
+                </UncontrolledCollapse>
+            </DropInfo>
             <div className="bottom-quote">
-                <p>"{props.qdata.quote}"</p>
-                <p>- {props.qdata.author}</p>
-                {/* <p>Date: {props.qdata.date}</p> */}
+                <Caption>
+                    <p>"{props.qdata.quote}"</p>
+                    <p>- {props.qdata.author}</p>
+                </Caption>
             </div>
         </div>
     );
 };
 export default PhotoCard;
+
